48ead8ea660c4cb183ab9340106bb1be91601ff9
MiguelMoreiraDev <contatomigueldev@gmail.com>
2026-02-04 23:03:31 -0300

    ✨ Feat: Create basic express server

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import cors from "cors"
+import express, { type ErrorRequestHandler } from "express"
+import helmet from "helmet"
+import { StatusCodes } from "http-status-codes"
+import { ZodError } from "zod"
+import { routes } from "@/routes.ts"
+
+export const app = express()
+
+app.use(cors({ origin: process.env.FRONTEND_URL }))
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use("/api", routes)
+app.use((_req, res) => {
+	return res.status(StatusCodes.NOT_FOUND).json({
+		message: "Rota não encontrada!",
+	})
+})
+const globalErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+	if (err instanceof ZodError) {
+		return res.status(StatusCodes.BAD_REQUEST).json({
+			message: err.message,
+		})
+	}
+
+	return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+		message: "Internal Server Error ❌",
+	})
+}
+app.use(globalErrorHandler)
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express"
+
+export const routes = Router()
+
+routes.get("/hello", (_req, res) => {
+	return res.status(200).json({
+		message: "Hello world",
+	})
+})
