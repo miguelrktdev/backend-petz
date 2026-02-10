449f4112d245891add306d083093307a8e0e0e05
MiguelMoreiraDev <contatomigueldev@gmail.com>
2026-02-10 15:45:46 -0300

    🩹 Fix: Fix JWT Token returning name

--- src/modules/auth/services/authenticate.service.ts
+++ src/modules/auth/services/authenticate.service.ts
@@ -1,64 +1,70 @@
-import type { PrismaUserRepository } from "@/modules/users/prisma-user.repository.ts"
-import { InvalidCredentialsError } from "@/shared/errors/invalid-credentials.error.ts"
-import { ResourceNotFoundError } from "@/shared/errors/resource-not-found.error.ts"
-import { UserNotVerifiedError } from "@/shared/errors/user-not-verified.error.ts"
-import type { GenerateJwtToken } from "@/shared/lib/generate-jwt-token.ts"
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import type { PrismaUserRepository } from "@/modules/users/prisma-user.repository.ts";
+import { InvalidCredentialsError } from "@/shared/errors/invalid-credentials.error.ts";
+import { ResourceNotFoundError } from "@/shared/errors/resource-not-found.error.ts";
+import { UserNotVerifiedError } from "@/shared/errors/user-not-verified.error.ts";
+import type { GenerateJwtToken } from "@/shared/lib/generate-jwt-token.ts";
 
 interface AuthenticateUserRequest {
-	email: string
-	password: string
+  email: string;
+  password: string;
 }
 
 interface AuthenticateUserResponse {
-	token: string
-	refreshToken: string
+  accessToken: string;
+  refreshToken: string;
 }
 
 export class UserAuthenticateService {
-	constructor(
-		private userRepository: PrismaUserRepository,
-		private generateJwtToken: GenerateJwtToken,
-	) {}
+  constructor(
+    private userRepository: PrismaUserRepository,
+    private generateJwtToken: GenerateJwtToken,
+  ) { }
 
-	async handle({ email, password }: AuthenticateUserRequest): Promise<AuthenticateUserResponse> {
-		const user = await this.userRepository.findByEmail(email)
+  async handle({
+    email,
+    password,
+  }: AuthenticateUserRequest): Promise<AuthenticateUserResponse> {
+    const user = await this.userRepository.findByEmail(email);
 
-		if (!user) {
-			throw new ResourceNotFoundError("Usuário")
-		}
+    if (!user) {
+      throw new ResourceNotFoundError("Usuário");
+    }
 
-		if (!user.email_verified_at) {
-			throw new UserNotVerifiedError()
-		}
+    if (!user.email_verified_at) {
+      throw new UserNotVerifiedError();
+    }
 
-		const doesPasswordMatches = await bcrypt.compare(password, user.password_hash)
+    const doesPasswordMatches = await bcrypt.compare(
+      password,
+      user.password_hash,
+    );
 
-		if (!doesPasswordMatches) {
-			throw new InvalidCredentialsError()
-		}
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
 
-		const { token } = this.generateJwtToken.handle({
-			payload: {
-				sub: user.id,
-			},
-			options: {
-				expiresIn: "15m",
-			},
-		})
+    const { token: accessToken } = this.generateJwtToken.handle({
+      payload: {
+        sub: user.id,
+      },
+      options: {
+        expiresIn: "15m",
+      },
+    });
 
-		const { token: refreshToken } = this.generateJwtToken.handle({
-			payload: {
-				sub: user.id,
-			},
-			options: {
-				expiresIn: "7d",
-			},
-		})
+    const { token: refreshToken } = this.generateJwtToken.handle({
+      payload: {
+        sub: user.id,
+      },
+      options: {
+        expiresIn: "7d",
+      },
+    });
 
-		return {
-			token,
-			refreshToken,
-		}
-	}
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
 }
